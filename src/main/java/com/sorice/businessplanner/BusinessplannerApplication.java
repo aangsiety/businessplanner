16a6c82ab4e6646fd1de0d6b15db5d0d75a83c3e
Anandu <anandudileep07@gmail.com>
2021-08-04 11:44:13 +0530

    commiting everything

---
+++ src/main/java/com/sorice/businessplanner/BusinessplannerApplication.java
@@ -0,0 +1,14 @@
+package com.sorice.businessplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessplannerApplication.class, args);
+		System.out.println("Businessplanner printing");
+	}
+
+}
---
+++ src/test/java/com/sorice/businessplanner/BusinessplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sorice.businessplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
